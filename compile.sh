67bf7c1e901fbaf022d647015662cdbb42442162
Davor Sauer <davor@backbase.com>
2018-01-28 16:06:20 +0100

    Adjust location for module-info.java

--- compile.sh
+++ compile.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 javac -d target/classes/com.hello.world.mars $(find ./com.hello.world.mars/ -name "*.java")
 javac -p target/classes -d target/classes/com.hello.world $(find ./com.hello.world/ -name "*.java")
